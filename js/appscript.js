bfdbf3813f160b36d43c2fecf6e9ddb99bf79dab
SC1314520 <g0710126@hchs.hc.edu.tw>
2023-05-17 16:54:18 +0800

    prevent index of data source from changing

--- js/appscript.js
+++ js/appscript.js
@@ -1,6 +1,5 @@
 const sheet = SpreadsheetApp.getActiveSpreadsheet().getSheetByName('All');
 const NTHU = SpreadsheetApp.getActiveSpreadsheet().getSheetByName('NTHU');
-const INDEX = SpreadsheetApp.getActiveSpreadsheet().getSheetByName('index');
 function doGet(){
   var data =NTHU.getDataRange().getValues();
   var output = JSON.stringify(data);
@@ -20,14 +19,12 @@ function initialInsert(){
     var name=data[i]['name_tw'];
     if(name.indexOf('清華大學')!=-1 ||name.indexOf('十八尖山')!=-1){
       NTHU.appendRow([i,data[i]['name_tw'],data[i]['empty_spaces'],data[i]['available_spaces']]);
-      INDEX.appendRow([i]);
     }
     else{
-      sheet.appendRow([data[i]['name_tw'],data[i]['empty_spaces'],data[i]['available_spaces']]);
+      //sheet.appendRow([data[i]['name_tw'],data[i]['empty_spaces'],data[i]['available_spaces']]);
     }
   }
 }
-
 // update
 function updateData(){
   const url =UrlFetchApp.fetch('https://apis.youbike.com.tw/json/station-yb2.json').getContentText();
@@ -35,37 +32,22 @@ function updateData(){
   var data = JSON.parse($.text());
   return data;
 }
-function getIndex(){
-  return INDEX.getRange(1,1,NTHU.getLastRow(),1).getValues();
-}
 function updateNTHU(){
   try{
-    var index = getIndex();
     clearData();
     var data =updateData();
-    var newData=[];
-    for(var i=0;i<index.length;i++){
-      newData.push(data[index[i][0]])
-      NTHU.appendRow([index[i][0],data[index[i][0]]['name_tw'],data[index[i][0]]['empty_spaces'],data[index[i][0]]['available_spaces']]);
-    }
+    initialInsert();
   }catch(e){
     initialInsert();
   }
 }
-
-
 // clear
 function clearData(){
-  NTHU.getDataRange().clearContent();
-}
-function clearPartialData(){
-  sheet.getDataRange().clearContent();
   NTHU.getDataRange().clearContent();
 }
 function clearAllData(){
   sheet.getDataRange().clearContent();
   NTHU.getDataRange().clearContent();
-  INDEX.getDataRange().clearContent();
 }
 
 // send
@@ -81,7 +63,6 @@ function sendMessage(){
     htmlBody:content
   })
 }
-
 function sendToUBIKE(){
   var flag=0;
   var content='<table style="border: 1px sold gray; width: 350px; text-align: center" rules="all"><tr><td colspan="3">清華大學</td></tr><tr><td colspan="3">'+new Date()+'</td></tr><tr><td>地點</td><td>空位</td><td>可借</td></tr>';
